89d097deb64ece3d7ce09f9d1cf583210af652b4
Zachary Soliman <zachsoliman1@gmail.com>
2023-03-14 14:25:42 -0400

    search component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav';
 import Title from './components/Title';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Title />
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+const Search = () => {
+    return (
+        <div>
+            <input placeholder="Search"></input>
+        </div>
+    )
+}
+
+export default Search;
