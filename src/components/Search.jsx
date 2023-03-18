3afb688e0e352c51cd54347b89a128f2422c7440
Zachary Soliman <zachsoliman1@gmail.com>
2023-03-18 15:41:31 -0400

    results display in console

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,26 +6,26 @@ const Search = () => {
     const API_key = '6d2d21f7549a1e994baf9c19bdbb84e4'
     const [searchTerm, setSearchTerm] = useState('')
 
-    // const searchMovies = async (searchTerm, pageNum) => {
+    const searchMovies = async (searchTerm, pageNum) => {
 
-    //     // searches movies using searchTerm and gets page number pageNum
-    //     const req = await fetch(
-    //         `${baseURL}/search/movie?api_key=${API_key}&language=en-US&query=${encodeURI(
-    //             searchTerm)}&page=${'1'}&include_adult=false`
-    //     );
-    //     const res = await req.json();
-    //     console.log(res)
-    //     return res;
-    // };
+        // searches movies using searchTerm and gets page number pageNum
+        const req = await fetch(
+            `${baseURL}/search/movie?api_key=${API_key}&language=en-US&query=${encodeURI(
+                searchTerm)}&page=${'1'}&include_adult=false`
+        );
+        const res = await req.json();
+        console.log(res)
+        return res;
+    };
 
     const handleSearch = (e) => {
         e.preventDefault()
         setSearchTerm(e.target.value)
-        console.log('1', e.target.value)
-        console.log('2', searchTerm)
+        // console.log('1', e.target.value)
+        // console.log('2', searchTerm)
     }
 
-    // searchMovies(searchTerm);
+    searchMovies(searchTerm);
 
     // useEffect(() => {
     //     searchMovies();
